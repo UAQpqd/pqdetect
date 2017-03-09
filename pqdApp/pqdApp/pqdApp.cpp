f569416533b44b84b774622225085da46f044529
Francisco José Solís Muñoz <siscomagma@gmail.com>
2017-03-08 19:06:46 -0600

    FFT and IFFT added.

--- pqdApp/pqdApp/pqdApp.cpp
+++ pqdApp/pqdApp/pqdApp.cpp
@@ -11,9 +11,13 @@
 #include <math.h>
 #include <algorithm>
 #include "SignalGenerator.h"
+#include <time.h>
 
 int main(int argc, char **argv)
 {
+	clock_t t1, t2;
+	t1 = clock();
+
 	printf(""\
 		"PQD Parallel Signal Analyzer\n"\
 		"------------------------------------------------\n"\
@@ -29,12 +33,23 @@ int main(int argc, char **argv)
 	SignalGenerator::WhiteNoiseSignalGenerator gen2 = SignalGenerator::WhiteNoiseSignalGenerator();
 	gen2.setMean(0).setStandardDeviation(10);
 	//Generate the sum of them
-	const size_t signalLength = 10000;
+	const size_t signalLength = 100000;
 	SignalObject signal1 = gen1.generate(signalLength);
 	SignalObject signal2 = gen2.generate(signalLength);
 	SignalObject sumSignal = signal1 + signal2;
-	sumSignal.addSagSwell(100, 300, 0.5);
-	sumSignal.writeCSV("sumSignal.csv");
+	sumSignal.addSagSwell(100, 199, 0.5);
+	double *ptr = &(*sumSignal.m_data)[0];
+	//Real signal has been created, estimate phase, max amplitude and frequency
+	af::array realSignal(400, ptr, afHost);
+	realSignal = af::fft(realSignal);
+
+	realSignal = af::ifft(realSignal);
+	//af::print("Real signal:", realSignal);
+	//sumSignal.writeCSV("sumSignal.csv");
+
+	t2 = clock();
+	float diff((float)t2 - (float)t1);
+	std::cout << "Running time: " << diff << std::endl;
 	system("PAUSE");
     return 0;
 }
